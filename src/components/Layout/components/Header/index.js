053853aecc7ab2aff4074213589955d5329585cf
Huy Huynh <htqhuy1762@gmail.com>
2023-08-14 23:17:37 +0700

    Build Header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,13 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless'; // different import path!
-import { useEffect, useState } from 'react';
+
+import Button from '~/components/Button'
+import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import styles from './Header.module.scss';
 import AccountItem from '~/components/AccoutItem';
 
 const cx = classNames.bind(styles);
@@ -53,7 +55,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('action')}></div>
+                <div className={cx('action')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
