1927b919a3947cacc02472efae457b0708b4565c
Huy Huynh <htqhuy1762@gmail.com>
2023-08-22 11:58:16 +0700

    Logic Header #2

--- src/components/AccoutItem/index.js
+++ src/components/AccoutItem/index.js
@@ -2,24 +2,24 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom'
 import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
-    return <div className={cx('wrapper')}>
-        <Image className={cx('avatar')} src='https://toigingiuvedep.vn/wp-content/uploads/2023/03/hinh-do-mixi-cat-toc-mai-ngo.jpg' alt='Mixigaming' />
-        <div className={cx('info')}>
-            <h4 className={cx('name')}>
-                <span>Mixigaming</span>
-                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-            </h4>
-            <span className={cx('username')}>
-                Độ Phùng
-            </span>
-
-        </div>
-    </div>;
+function AccountItem({ data }) {
+    return (
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
+                </h4>
+                <span className={cx('username')}>{data.nickname}</span>
+            </div>
+        </Link>
+    );
 }
 
 export default AccountItem;
