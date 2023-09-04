ef585f4038d283aa9568c01d2c5d2285d45569e0
Huy Huynh <htqhuy1762@gmail.com>
2023-09-04 22:14:31 +0700

    UI Sidebar #3

--- src/layouts/components/SuggestedAccounts/AccountItem.js
+++ src/layouts/components/SuggestedAccounts/AccountItem.js
@@ -2,24 +2,43 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './SuggestedAccounts.module.scss';
+import AccountPreview from './AccountPreview/AccountPreview';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return ( 
-        <div className={cx('account-item')}>
-            <img className={cx('avatar')} src='https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/1212f6150780cd4a82ab13104c7c431b~c5_100x100.jpeg?x-expires=1693576800&x-signature=f9gZXGDzr8xd3KtOXWMrQRdqv78%3D' alt='' />
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>
-                        CR7Siuuu
-                    </strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>Cristiano Ronaldo</p>
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
             </div>
+        );
+    };
+
+    return (
+        <div>
+            <Tippy interactive delay={[800, 0]} offset={[-35, 0]} placement="bottom-end" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://cdnimg.vietnamplus.vn/uploaded/mzdic/2023_03_24/cristiano_ronaldo_portugal_2403.jpg"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>CR7Siuuu</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Cristiano Ronaldo</p>
+                    </div>
+                </div>
+            </Tippy>
         </div>
     );
 }
---
+++ src/layouts/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,37 @@
+import classNames from 'classnames/bind';
+import styles from './AccountPreview.module.scss';
+import Button from '~/components/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <header className={cx('header')}>
+                <img
+                    className={cx('avatar')}
+                    src="https://cdnimg.vietnamplus.vn/uploaded/mzdic/2023_03_24/cristiano_ronaldo_portugal_2403.jpg"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>Follow</Button>
+            </header>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>CR7Siuuu</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Cristiano Ronaldo</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Followers</span>
+                    <strong className={cx('value')}>446.9M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
